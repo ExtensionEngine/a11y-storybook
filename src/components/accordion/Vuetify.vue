717321f0a515961eccc364b960375436430def6f
Marko Jevtic <marjevtic@gmail.com>
2022-07-25 15:39:43 +0200

    Move vuetify accordion metadata presentation from component to story

--- src/components/accordion/Vuetify.vue
+++ src/components/accordion/Vuetify.vue
@@ -1,23 +1,15 @@
 <template>
-  <div>
-    <h1 class="mb-3 teal--text">Vuetify 2 Accordion</h1>
-    <h2 class="mb-2 teal--text">Example</h2>
-    <v-expansion-panels>
-      <v-expansion-panel v-for="(item,i) in 5" :key="i">
-        <v-expansion-panel-header>
-          <h3>Item</h3>
-        </v-expansion-panel-header>
-        <v-expansion-panel-content>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
-    <p class="mt-5">
-      Source:
-      <a href="https://vuetifyjs.com/en/components/expansion-panels/" target="_blank">Vuetify Expansion panels</a>
-    </p>
-  </div>
+  <v-expansion-panels>
+    <v-expansion-panel v-for="(item,i) in 5" :key="i">
+      <v-expansion-panel-header>
+        <h3>Item</h3>
+      </v-expansion-panel-header>
+      <v-expansion-panel-content>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-expansion-panels>
 </template>
 
 <script>
--- src/stories/Accordion.stories.js
+++ src/stories/Accordion.stories.js
@@ -15,46 +15,47 @@ export default {
 const VanillaTemplate = () => ({
   components: { VanillaAccordion },
   template: `
-  <div>
-    <h1 class="mb-3 teal--text">Vanilla Accordion</h1>
+    <div>
+      <h1 class="mb-3 teal--text">Vanilla Accordion</h1>
 
-    <v-alert border="bottom" colored-border type="warning" elevation="2">
-      <p>
-        The code in this example is not intended for production environments.
-        Before using it for any purpose, read this to understand why.
-      </p>
-      <p>This is an illustrative example of one way of using ARIA that conforms with the ARIA specification.</p>
+      <v-alert border="bottom" colored-border type="warning" elevation="2">
+        <p>
+          The code in this example is not intended for production environments.
+          Before using it for any purpose, read this to understand why.
+        </p>
+        <p>This is an illustrative example of one way of using ARIA that conforms with the ARIA specification.</p>
 
-      <ul>
-        <li>
-          There may be support gaps in some
-          <a href="https://www.w3.org/WAI/ARIA/apg/practices/read-me-first/#browser_and_AT_support" target="_blank">browser and assistive technology combinations</a>,
-          especially for
-          <a href="https://www.w3.org/WAI/ARIA/apg/practices/read-me-first/#mobile_and_touch_support" target="_blank">mobile/touch devices</a>.
-          Testing code based on this example with assistive technologies is essential before considering use in production systems.
-        </li>
-        <li>
-          The
-          <a href="https://aria-at.w3.org" target="_blank">ARIA and Assistive Technologies Project</a>
-          is developing measurements of assistive technology support for APG examples.
-        </li>
-        <li>
-          Robust accessibility can be further optimized by choosing implementation patterns that
-          <a href="https://www.w3.org/TR/using-aria/#rule1" target="_blank">maximize use of semantic HTML</a>
-          and heeding the warning that
-          <a href="https://www.w3.org/WAI/ARIA/apg/practices/read-me-first/#no_aria_better_bad_aria" target="_blank">No ARIA is better than Bad ARIA</a>.
-        </li>
-      </ul>
-      <p>
-        Source:
-        <a href="https://www.w3.org/WAI/ARIA/apg/example-index/accordion/accordion.html" target="_blank">ARIA Authoring Practices Guide (APG)</a>
-      </p>
-    </v-alert>
+        <ul>
+          <li>
+            There may be support gaps in some
+            <a href="https://www.w3.org/WAI/ARIA/apg/practices/read-me-first/#browser_and_AT_support" target="_blank">browser and assistive technology combinations</a>,
+            especially for
+            <a href="https://www.w3.org/WAI/ARIA/apg/practices/read-me-first/#mobile_and_touch_support" target="_blank">mobile/touch devices</a>.
+            Testing code based on this example with assistive technologies is essential before considering use in production systems.
+          </li>
+          <li>
+            The
+            <a href="https://aria-at.w3.org" target="_blank">ARIA and Assistive Technologies Project</a>
+            is developing measurements of assistive technology support for APG examples.
+          </li>
+          <li>
+            Robust accessibility can be further optimized by choosing implementation patterns that
+            <a href="https://www.w3.org/TR/using-aria/#rule1" target="_blank">maximize use of semantic HTML</a>
+            and heeding the warning that
+            <a href="https://www.w3.org/WAI/ARIA/apg/practices/read-me-first/#no_aria_better_bad_aria" target="_blank">No ARIA is better than Bad ARIA</a>.
+          </li>
+        </ul>
+        <p>
+          Source:
+          <a href="https://www.w3.org/WAI/ARIA/apg/example-index/accordion/accordion.html" target="_blank">ARIA Authoring Practices Guide (APG)</a>
+        </p>
+      </v-alert>
 
-    <h2 class="mb-2 teal--text">Example</h2>
+      <h2 class="mb-2 teal--text">Example</h2>
 
-    <vanilla-accordion />
-  </div>`,
+      <vanilla-accordion />
+    </div>
+  `,
 });
 
 export const VanillaImplementation = VanillaTemplate.bind({});
@@ -65,7 +66,19 @@ VanillaImplementation.play = async () => {
 
 const VuetifyTemplate = () => ({
   components: { VuetifyAccordion },
-  template: '<vuetify-accordion/>',
+  template: `
+    <div>
+      <h1 class="mb-3 teal--text">Vuetify 2 Accordion</h1>
+      <h2 class="mb-2 teal--text">Example</h2>
+
+      <vuetify-accordion/>
+
+      <p class="mt-5">
+        Source:
+        <a href="https://vuetifyjs.com/en/components/expansion-panels/" target="_blank">Vuetify Expansion panels</a>
+      </p>
+    </div>
+  `,
 });
 export const Vuetify2Implementation = VuetifyTemplate.bind({});
 Vuetify2Implementation.play = async () => {
